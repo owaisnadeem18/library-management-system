8ceb8443b9c1ea099122a73a16be35593598b353
owaisnadeem18 <108211200+owaisnadeem18@users.noreply.github.com>
2026-09-02 14:22:37 +0500

    issue book controller API

--- backend/controllers/borrowController.js
+++ backend/controllers/borrowController.js
@@ -1,6 +1,14 @@
+import { BorrowService } from "../services/borrowService";
 import { catchAsync } from "../utils/catchAsync";
 
 export const issueBook = catchAsync(async (req , res , next) => { 
     const { book_id } = req.body;
-  const userId = req.user.id
+    const userId = req.user.id
+
+    if (!book_id) return next(new AppError('Book ID is required.', 400));
+
+    const result = await BorrowService.issueBook(userId, book_id);
+
+    res.status(201).json({ status: 'success', data: { borrow: result } });
+
 })
