8a338fb59e68e200044668d978b7aafc5e996f3d
Owais Nadeem <owaisnadeem15@gmail.com>
2026-09-04 23:56:18 +0500

    Update borrowController.js

--- backend/controllers/borrowController.js
+++ backend/controllers/borrowController.js
@@ -5,6 +5,7 @@ export const issueBook = catchAsync(async (req , res , next) => {
     const { book_id } = req.body;
     const userId = req.user.id
 
+    
     if (!book_id) return next(new AppError('Book ID is required.', 400));
 
     const result = await BorrowService.issueBook(userId, book_id);
